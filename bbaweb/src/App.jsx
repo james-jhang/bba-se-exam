5e4c36ca8a33c6b8bcd884f3adeefe95f5c24a5f
James Jhang <e8315402@gmail.com>
2023-07-05 17:26:26 +0800

    add react router

--- bbaweb/src/App.jsx
+++ bbaweb/src/App.jsx
@@ -1,39 +1,24 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+import { Outlet, Link } from "react-router-dom";
 
-import UserList from './user-list/user-list'
-import Counter from './counter/counter'
-function App() {
-  // const [count, setCount] = useState(0)
 
+export default function Root() {
   return (
     <>
-      <UserList></UserList>
-      <Counter></Counter>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="sidebar">
+        <nav>
+          <ul>
+            <li>
+              <Link to={`/users`}>Users</Link>
+            </li>
+            <li>
+              <Link to={`/counter`}>Counter</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div id="detail">
+        <Outlet />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
     </>
-  )
+  );
 }
-
-export default App
--- bbaweb/src/main.jsx
+++ bbaweb/src/main.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
 import App from './App.jsx'
+import ErrorPage from './error-page.jsx'
+import UserList from './user-list/user-list'
+import Counter from './counter/counter'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "users",
+        element: <UserList />,
+      },
+      {
+        path: "counter",
+        element: <Counter />,
+      }
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
